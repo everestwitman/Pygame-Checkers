9871997f8b9991451b1b7773d068166fed4130a6
quyq <dummyfk@gmail.com>
2023-03-13 22:46:02 -0400

    Fix hop remove piece
    Fix bug introduced while coverting
    Python2 div to Python3 int div round

--- checkers.py
+++ checkers.py
@@ -98,7 +98,7 @@ class Game:
 						self.board.move_piece(self.selected_piece, self.mouse_pos)
 					
 						if self.mouse_pos not in self.board.adjacent(self.selected_piece):
-							self.board.remove_piece((self.selected_piece[0] + (self.mouse_pos[0] - self.selected_piece[0]) >> 1, self.selected_piece[1] + (self.mouse_pos[1] - self.selected_piece[1]) >> 1))
+							self.board.remove_piece(((self.selected_piece[0] + self.mouse_pos[0]) >> 1, (self.selected_piece[1] + self.mouse_pos[1]) >> 1))
 						
 							self.hop = True
 							self.selected_piece = self.mouse_pos
@@ -109,7 +109,7 @@ class Game:
 				if self.hop == True:					
 					if self.selected_piece != None and self.mouse_pos in self.board.legal_moves(self.selected_piece, self.hop):
 						self.board.move_piece(self.selected_piece, self.mouse_pos)
-						self.board.remove_piece((self.selected_piece[0] + (self.mouse_pos[0] - self.selected_piece[0]) >> 1, self.selected_piece[1] + (self.mouse_pos[1] - self.selected_piece[1]) >> 1))
+						self.board.remove_piece(((self.selected_piece[0] + self.mouse_pos[0]) >> 1, (self.selected_piece[1] + self.mouse_pos[1]) >> 1))
 
 					if self.board.legal_moves(self.mouse_pos, self.hop) == []:
 							self.end_turn()
